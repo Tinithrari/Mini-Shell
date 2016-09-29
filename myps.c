47e3e1302af968b60efc7597dbda73f92cb4f961
Xavier Heugue <xavier_heugue@ens.univ-artois.fr>
2016-09-29 11:48:58 +0200

    myps implemented

--- myps.c
+++ myps.c
@@ -7,18 +7,30 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define ERR -1
 #define BUFFER_SIZE 512
 
-#define SKIP_REF(dir) readdir( dir ), readdir( dir )
 #define syserror(s, c) perror(s), exit(c)
 
 #define PROC_FOLDER "/proc"
 
+int isNumber(char* s)
+{
+	char *c;
+
+	if (s == NULL)
+		return 0;
+
+	for (c = s; (*c) && (*c >= '0' && *c <= '9') ; c++);
+
+	return !(*c);
+}
+
 int main(void)
 {
-	DIR *procDir, *pidDir;
+	DIR *procDir;
 	struct dirent *processus;
 
 	procDir = opendir( PROC_FOLDER );
@@ -26,16 +38,18 @@ int main(void)
 	if (procDir == NULL)
 		syserror("Directory error", 1);
 
-	SKIP_REF(procDir);
 
 	printf ("PID\tCMD\n");
 
 	for ( processus = readdir( procDir ); processus; processus = readdir( procDir ) )
 	{
 		struct stat* status;
-		char buffer[BUFFER_SIZE];
+		char buffer[BUFFER_SIZE], c;
 		int fd;
 		ssize_t size;
+
+		if (! isNumber(processus->d_name))
+			continue;
 
 		printf("%s\t", processus->d_name);
 /*
@@ -50,29 +64,27 @@ int main(void)
 
 		printf("%d:%d:%d\t", status->st_mtim.tm_hour, status->st_mtim.tm_min, status-> st_mtim.tm_sec);
 */
-		sprintf(buffer, "%s/%s/cmdline", procDir->d_name, processus->d_name);
+
+		sprintf(buffer, "%s/%s/cmdline", PROC_FOLDER, processus->d_name);
+
 		fd = open(buffer, O_RDONLY);
 
 		if (fd == ERR)
 		{
 			closedir(procDir);
-			closedir(processus);
-			syserror("Open error", 3);
+			syserror(buffer, 3);
 		}
-		
-		for (; (size = read(fd, buffer, BUFFER_SIZE)) == BUFFER_SIZE ;)
-			printf("%s", buffer);
+	
+		memset(buffer, 0, BUFFER_SIZE);
+	
+		for (; (size = read(fd, &c, sizeof(char))) > 0;)
+			putchar(c);
 
-		close(fd);
-
-		if (size)
-		{
-			closedir(procDir);
-			closedir(processus);
+		if (size == ERR)
 			syserror("Read error", 4);
-		}
+		
+		close(fd);
 		putchar('\n');
-		closedir(processus);
 	}
-	closedir(procdir);
+	closedir(procDir);
 }
