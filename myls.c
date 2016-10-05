72e6c63413a92909683fcacbcd1b16bea2de5a91
Ydee Nicolas <nicolas_ydee@ens.univ-artois.fr>
2016-10-05 09:44:51 +0200

    new mise a jour myls

--- myls.c
+++ myls.c
@@ -10,13 +10,14 @@
 
 #define ERR -1
 #define syserror(m,e) perror(m), exit(e)
+#define fatalerror(m,e) fprintf(stderr, "%s\n", m), exit(e)
 
 #define FLAG_A 0
 #define FLAG_R 1
 
 int main(int argc, char *argv[])
 {
-	char flag[2] = {0,0};
+	char flag[2] = {0,0}, bufferErr[128];
 	char *s;
 	DIR* d; 		//repertoire
 	struct dirent *elt;	//content
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
 	argc--;
 	argv++;
 	
-	for (;argc;)
+	for (;argc && (**argv) == '-';)
 	{
 		if ( ! strcmp("-aR", *(argv)) || ! strcmp("-Ra", *(argv)) )
 		{
@@ -36,7 +37,10 @@ int main(int argc, char *argv[])
 		else if (! strcmp("-R", *(argv)))
 			flag[FLAG_R] = 1;
 		else
-			break;
+		{
+			sprintf(bufferErr, "Option invalide : %s", *(argv));
+			fatalerror(bufferErr, 2);
+		}
 		argc--; argv++;
 	}
 	
