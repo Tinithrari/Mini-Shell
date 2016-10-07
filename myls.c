0216edf6f9f1b3ce538f564210bd912f7890f72a
Nicolas Ydée <nicolas_ydee@ens.univ-artois.fr>
2016-10-07 10:25:06 +0200

    myls -l quasi operationel

--- myls.c
+++ myls.c
@@ -4,6 +4,8 @@
 #include<dirent.h>
 #include<time.h>
 #include<string.h>
+#include<unistd.h>
+
 
 #include<sys/types.h>
 #include<sys/stat.h>
@@ -21,7 +23,8 @@ int main(int argc, char *argv[])
 	char *s;
 	DIR* d; 		//repertoire
 	struct dirent *elt;	//content
-	
+	struct stat st;
+
 	argc--;
 	argv++;
 	
@@ -50,12 +53,52 @@ int main(int argc, char *argv[])
 		
 		printf("%s : \n",s);
 		
-		while(elt = readdir(d)) printf("%s \t", elt->d_name);
-		
+		while(elt = readdir(d))
+		{ 
+			if(stat(elt->d_name,&st) == ERR)
+				syserror("Stat error",2);
+			
+			printf("%s \t", elt->d_name);
+
+			printf("File type:                ");
+
+           switch (st.st_mode & S_IFMT) {
+           case S_IFBLK:  printf("block device\n");            break;
+           case S_IFCHR:  printf("character device\n");        break;
+           case S_IFDIR:  printf("directory\n");               break;
+           case S_IFIFO:  printf("FIFO/pipe\n");               break;
+           case S_IFLNK:  printf("symlink\n");                 break;
+           case S_IFREG:  printf("regular file\n");            break;
+           case S_IFSOCK: printf("socket\n");                  break;
+           default:       printf("unknown?\n");                break;
+           }
+
+           printf("I-node number:            %ld\n", (long) st.st_ino);
+
+           printf("Mode:                     %lo (octal)\n",
+                   (unsigned long) st.st_mode);
+
+           printf("Link count:               %ld\n", (long) st.st_nlink);
+           printf("Ownership:                UID=%ld   GID=%ld\n",
+                   (long) st.st_uid, (long) st.st_gid);
+
+           printf("Preferred I/O block size: %ld bytes\n",
+                   (long) st.st_blksize);
+           printf("File size:                %lld bytes\n",
+                   (long long) st.st_size);
+           printf("Blocks allocated:         %lld\n",
+                   (long long) st.st_blocks);
+
+           printf("Last status change:       %s", ctime(&st.st_ctime));
+           printf("Last file access:         %s", ctime(&st.st_atime));
+           printf("Last file modification:   %s \n", ctime(&st.st_mtime));
+
+		}
+
 		closedir(d);
 		
-		putchar('\n');
-		
+		//putchar('\n');
+
 		if  (!argc) break;
 		
 		if(s=*(++argv))putchar('\n');
