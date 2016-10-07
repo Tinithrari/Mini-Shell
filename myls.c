9f5fe1000a0bfa628f4248ed422f1b8bf1c6567b
Nicolas Ydée <nicolas_ydee@ens.univ-artois.fr>
2016-10-07 11:17:24 +0200

    ajout des droits pour myls

--- myls.c
+++ myls.c
@@ -6,7 +6,6 @@
 #include<string.h>
 #include<unistd.h>
 
-
 #include<sys/types.h>
 #include<sys/stat.h>
 
@@ -57,42 +56,32 @@ int main(int argc, char *argv[])
 		{ 
 			if(stat(elt->d_name,&st) == ERR)
 				syserror("Stat error",2);
-			
-			printf("%s \t", elt->d_name);
 
-			printf("File type:                ");
-
-           switch (st.st_mode & S_IFMT) {
-           case S_IFBLK:  printf("block device\n");            break;
-           case S_IFCHR:  printf("character device\n");        break;
-           case S_IFDIR:  printf("directory\n");               break;
-           case S_IFIFO:  printf("FIFO/pipe\n");               break;
-           case S_IFLNK:  printf("symlink\n");                 break;
-           case S_IFREG:  printf("regular file\n");            break;
-           case S_IFSOCK: printf("socket\n");                  break;
-           default:       printf("unknown?\n");                break;
+           switch (st.st_mode & S_IFMT) 
+		   {
+           		case S_IFDIR: 
+				    printf("d");               
+					break;
+           		default:       
+				   	printf("-");               
+				   	break;
            }
 
-           printf("I-node number:            %ld\n", (long) st.st_ino);
+		    printf("%c",(st.st_mode & S_IRUSR) ? 'r' : '-' );
+			printf("%c",(st.st_mode & S_IWUSR) ? 'w' : '-' );
+		    printf("%c",(st.st_mode & S_IXUSR) ? 'x' : '-' );
+		    printf("%c",(st.st_mode & S_IRGRP) ? 'r' : '-' );
+		    printf("%c",(st.st_mode & S_IWGRP) ? 'w' : '-' );
+		    printf("%c",(st.st_mode & S_IXGRP) ? 'x' : '-' );
+			printf("%c",(st.st_mode & S_IROTH) ? 'r' : '-' );
+			printf("%c",(st.st_mode & S_IWOTH) ? 'w' : '-' );
+		 	printf("%c ",(st.st_mode & S_IXOTH) ? 'x' : '-' );
+		  
+        	printf("%lld ",(long long) st.st_size);
 
-           printf("Mode:                     %lo (octal)\n",
-                   (unsigned long) st.st_mode);
+        	//printf("%s ", ctime(&st.st_mtime));
 
-           printf("Link count:               %ld\n", (long) st.st_nlink);
-           printf("Ownership:                UID=%ld   GID=%ld\n",
-                   (long) st.st_uid, (long) st.st_gid);
-
-           printf("Preferred I/O block size: %ld bytes\n",
-                   (long) st.st_blksize);
-           printf("File size:                %lld bytes\n",
-                   (long long) st.st_size);
-           printf("Blocks allocated:         %lld\n",
-                   (long long) st.st_blocks);
-
-           printf("Last status change:       %s", ctime(&st.st_ctime));
-           printf("Last file access:         %s", ctime(&st.st_atime));
-           printf("Last file modification:   %s \n", ctime(&st.st_mtime));
-
+			printf("%s \n", elt->d_name);
 		}
 
 		closedir(d);
