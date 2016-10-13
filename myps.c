15a71046b603933750ae2a68327c4a5a498e9041
Xavier Heugue <xavier_heugue@ens.univ-artois.fr>
2016-10-13 10:26:20 +0200

    Getting more data

--- myps.c
+++ myps.c
@@ -14,7 +14,22 @@
 
 #define syserror(s, c) perror(s), exit(c)
 
-#define PROC_FOLDER "/proc"
+#define PROC_FOLDER "/proc/"
+#define PROC_STAT "/stat"
+
+typedef struct
+{
+	int uid;
+	int pid;
+	float cpu;
+	float mem;
+	unsigned long vsz;
+	long int rss;
+	int tty;
+	char stat;
+	unsigned long long start;
+	unsigned long time;
+} ProcInfo;
 
 /**
  * @fn int isNumber(char *s)
@@ -22,7 +37,7 @@
  * @param s the string to check
  * @return 1 if the string is a number, 0 otherwise
  */
-int isNumber(char* s)
+static int isNumber(char* s)
 {
 	char *c;
 
@@ -38,47 +53,50 @@ int main(void)
 {
 	DIR *procDir;
 	struct dirent *processus;
+	ProcInfo info;
 
 	procDir = opendir( PROC_FOLDER );
 
 	if (procDir == NULL)
 		syserror("Directory error", 1);
 
-
-	printf ("USR\tPID\t%cCPU\t%cMEM\tVSZ\tRSS\tTTY\tSTAT\tSTART\tTIME\tCOMMAND\n");
+	// Display Header
+	printf ("USR\tPID\t%cCPU\t%cMEM\tVSZ\tRSS\tTTY\tSTAT\tSTART\tTIME\tCOMMAND\n", '%', '%');
 
 	for ( processus = readdir( procDir ); processus; processus = readdir( procDir ) )
 	{
-		struct stat* status;
-		char buffer[BUFFER_SIZE], c;
-		int fd;
-		ssize_t size;
+		FILE* file;
+		unsigned long utime, stime;
 
+		// Check if the directory name is a PID
 		if (! isNumber(processus->d_name))
 			continue;
-
-		printf("%s\t", processus->d_name);
-
-		sprintf(buffer, "%s/%s/cmdline", PROC_FOLDER, processus->d_name);
-
-		fd = open(buffer, O_RDONLY);
-
-		if (fd == ERR)
-		{
-			closedir(procDir);
-			syserror(buffer, 3);
-		}
-	
-		memset(buffer, 0, BUFFER_SIZE);
-	
-		for (; (size = read(fd, &c, sizeof(char))) > 0;)
-			putchar(c);
-
-		if (size == ERR)
-			syserror("Read error", 4);
 		
-		close(fd);
-		putchar('\n');
+		// Open the file descriptor to the stat file
+		file = open(PROC_FOLDER + processus->d_name  + PROC_STAT, "r");
+
+		// Get pid, stat and tty
+		fscanf(file, "%d%s*%c%d*%d*%d*%d", &(info.pid), &(info.stat), &(info.tty));
+
+		// Skip argument
+		fscanf(file, "%d*%u*%lu*%lu*%lu*%lu*");
+		
+		// Get Time
+		fscanf("%lu%lu", &utime, &stime);
+
+		info.time = utime + stime;
+
+		// Skip other arguments
+		fscanf(file, "%ld*%ld*%ld*%ld*%ld*%ld*");
+
+		// Get start time, vsize, and rss
+		fscanf(file, "%llu%lu%ld", &(info.start), &(info.vsize), &(info.rss));
+
+		// close the stat file
+		fclose(file);
+
+		// Display PID
+		printf("%s\t", processus->d_name);
 	}
 	closedir(procDir);
 }
