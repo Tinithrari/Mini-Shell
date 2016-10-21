a7676d128a2a2312a87ba87b14025b6db5f911a6
Xavier Heugue <xavier_heugue@ens.univ-artois.fr>
2016-10-22 00:51:11 +0200

    Adding some informations in myps and creating a function

--- myps.c
+++ myps.c
@@ -1,6 +1,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/sysinfo.h>
 #include <dirent.h>
 
 #include <stdio.h>
@@ -8,6 +9,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+#include <pwd.h>
 
 #define ERR -1
 #define BUFFER_SIZE 512
@@ -16,6 +19,11 @@
 
 #define PROC_FOLDER "/proc/"
 #define PROC_STAT "/stat"
+#define PROC_STATUS "/status"
+
+#define LINE_BEFORE_UID 7
+
+struct sysinfo sysInfo;
 
 typedef struct
 {
@@ -49,12 +57,75 @@ static int isNumber(char* s)
 	return !(*c);
 }
 
+static ProcInfo getProcessusInformation(struct dirent* processus)
+{
+	FILE *file, *statusFile;
+	unsigned long utime, stime;
+	long int waitTimeU, waitTimeS;
+	ProcInfo info;
+	char buffer[256];
+	struct stat procStat;
+	int i;
+
+	sprintf(buffer, "%s%s%s", PROC_FOLDER, processus->d_name, PROC_STAT);
+	// Open the file descriptor to the stat file
+	file = fopen(buffer, "r");
+	stat(buffer, &procStat);
+
+	// Get pid, stat and tty
+	fscanf(file, "%d%*s%c%*d%*d%*d%d", &(info.pid), &(info.stat), &(info.tty));
+
+	// Skip argument
+	fscanf(file, "%*d%*u%*lu%*lu%*lu%*lu");
+		
+	// Get Time
+	fscanf(file, "%lu%lu%ld%ld", &utime, &stime, &waitTimeU, &waitTimeS);
+
+	info.time = utime + stime;
+
+	// Skip other arguments
+	fscanf(file, "%*ld%*ld%*ld%*ld");
+
+	// Get start time, vsize, and rss
+	fscanf(file, "%llu%lu%ld", &(info.start), &(info.vsz), &(info.rss));
+
+	// Compute the cpu usage percentage
+	info.cpu = (info.time / (info.time + waitTimeU + waitTimeS)) * 100;
+	info.time /= CLOCKS_PER_SEC;
+	info.start /= CLOCKS_PER_SEC;
+	
+
+	// Compute the memory usage percentage
+	info.mem = ( ( (float) info.rss ) / sysInfo.totalram ) * 100;
+
+	sprintf(buffer, "%s%s%s", PROC_FOLDER, processus->d_name, PROC_STATUS);
+	statusFile = fopen(buffer, "r");
+
+	// Skip Lines
+	for (i = 0; i < LINE_BEFORE_UID; i++)
+		fscanf(statusFile, "%*[^\n]\n");
+
+	// get uid
+	fscanf(statusFile, "%*[^0-9]%d", &(info.uid));
+
+	// close the stat file and the status file
+	fclose(file);
+	fclose(statusFile);
+
+	return info;
+}
+
 int main(void)
 {
 	DIR *procDir;
 	struct dirent *processus;
 	ProcInfo info;
 
+	// Get system information
+	if (sysinfo(&sysInfo) == ERR)
+		syserror("sysinfo error", 1);
+
+	// Open the proc folder
 	procDir = opendir( PROC_FOLDER );
 
 	if (procDir == NULL)
@@ -65,38 +136,15 @@ int main(void)
 
 	for ( processus = readdir( procDir ); processus; processus = readdir( procDir ) )
 	{
-		FILE* file;
-		unsigned long utime, stime;
-
 		// Check if the directory name is a PID
 		if (! isNumber(processus->d_name))
 			continue;
 		
-		// Open the file descriptor to the stat file
-		file = open(PROC_FOLDER + processus->d_name  + PROC_STAT, "r");
+		info = getProcessusInformation(processus);
 
-		// Get pid, stat and tty
-		fscanf(file, "%d%s*%c%d*%d*%d*%d", &(info.pid), &(info.stat), &(info.tty));
-
-		// Skip argument
-		fscanf(file, "%d*%u*%lu*%lu*%lu*%lu*");
-		
-		// Get Time
-		fscanf("%lu%lu", &utime, &stime);
-
-		info.time = utime + stime;
-
-		// Skip other arguments
-		fscanf(file, "%ld*%ld*%ld*%ld*%ld*%ld*");
-
-		// Get start time, vsize, and rss
-		fscanf(file, "%llu%lu%ld", &(info.start), &(info.vsize), &(info.rss));
-
-		// close the stat file
-		fclose(file);
-
-		// Display PID
-		printf("%s\t", processus->d_name);
+		printf("%s\t%d\t%.1f\t%.1f\t%lu\t%ld\t%d\t%c\t%llu\t%lu\n", getpwuid(info.uid)->pw_name, info.pid, info.cpu, info.mem, info.vsz, info.rss, info.tty, info.stat, info.start, info.time);
 	}
 	closedir(procDir);
+
+	exit(0);
 }
